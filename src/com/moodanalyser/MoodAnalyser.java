befd1bbb9b96db3a0f1b9eca037c3e6661d2e5b6
Kritika <kritsme963@gmail.com>
2022-12-07 02:13:58 -0800

    Added:handle exception if invalid mood is given

--- src/com/moodanalyser/MoodAnalyser.java
+++ src/com/moodanalyser/MoodAnalyser.java
@@ -3,9 +3,18 @@ package com.moodanalyser;
 public class MoodAnalyser {
 
     public String analyzeMood(String message) {
-        if(message.contains("Sad"))
-        return null;
+        //UC2-HANDLE EXCEPTION IF USER PROVIDE INVALID MOOD LIKE NULL
+        try{
+            String mood=null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        if (message.contains("Sad"))
+            return null;
         else
             return "Happy";
     }
 }
+
+
+
